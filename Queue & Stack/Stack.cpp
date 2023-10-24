25c7af6a1d6be095eeb82d70b19d705b3bb2b1ad
Rutu Bhanderi <140520663+rutubhanderi@users.noreply.github.com>
2023-10-24 19:00:39 +0530

    Update Stack.cpp

--- Queue & Stack/Stack.cpp
+++ Queue & Stack/Stack.cpp
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX_SIZE 100
 
-// Structure to represent the stack
 struct Stack {
     int arr[MAX_SIZE];
     int top;
 };
 
-// Function to initialize an empty stack
 void initialize(struct Stack *stack) {
     stack->top = -1;
 }
 
-// Function to check if the stack is empty
 int isEmpty(struct Stack *stack) {
     return (stack->top == -1);
 }
 
-// Function to check if the stack is full
 int isFull(struct Stack *stack) {
     return (stack->top == MAX_SIZE - 1);
 }
 
-// Function to push an element onto the stack
 void push(struct Stack *stack, int data) {
     if (isFull(stack)) {
         printf("Stack Overflow: Cannot push element onto the stack.\n");
@@ -43,7 +37,7 @@ int pop(struct Stack *stack) {
     return stack->arr[stack->top--];
 }
 
-// Function to display the contents of the stack
+
 void display(struct Stack *stack) {
     if (isEmpty(stack)) {
         printf("The stack is empty.\n");
